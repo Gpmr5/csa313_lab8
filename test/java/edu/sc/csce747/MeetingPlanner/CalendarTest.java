2cddddb1d99fd5c64587a4098605d976bcdd7ed5
Gpmr5 <ganaa2z2t@gmail.com>
2025-10-27 08:50:17 +0800

    add setUp and testMeetingConflict

--- test/java/edu/sc/csce747/MeetingPlanner/CalendarTest.java
+++ test/java/edu/sc/csce747/MeetingPlanner/CalendarTest.java
@@ -12,6 +12,11 @@ import org.junit.Test;
 public class CalendarTest {
 	// Add test methods here. 
 	// You are not required to write tests for all classes.
+	private Calendar calendar;
+	@Before
+	public void setUp() {
+    	calendar = new Calendar();
+	}
 	@Test
 	public void testAddMeeting_holiday() {
 		// Create Midsommar holiday
@@ -26,5 +31,19 @@ public class CalendarTest {
 			fail("Should not throw exception: " + e.getMessage());
 		}
 	}
+	public testAddMeeting_conflict(){
+		try {
+			Meeting meeting1 = new Meeting(5,10,10,12, "Team Sync");
+			Meeting meeting2 = new Meeting(5,10,11,13, "Client call");
+			calendar.addMeeting(meeting1);
+			calendar.addMeeting(meeting2);
+			fail("Should throw TimeConflictException due to overlap");
+
+		} catch (TimeConflictException e) {
+			assertTrue(e.getMessage().contains("Overlap with another item"));
+		}
+	}
+
+
 	
 }
