d1d8bc23196aa9be71903d729d389047aff4807c
Gpmr5 <ganaa2z2t@gmail.com>
2025-10-27 08:34:08 +0800

    first test

--- test/java/edu/sc/csce747/MeetingPlanner/CalendarTest.java
+++ test/java/edu/sc/csce747/MeetingPlanner/CalendarTest.java
@@ -12,5 +12,19 @@ import org.junit.Test;
 public class CalendarTest {
 	// Add test methods here. 
 	// You are not required to write tests for all classes.
+	@Test
+	public void testAddMeeting_holiday() {
+		// Create Midsommar holiday
+		// Add to calendar object.
+		try {
+			Meeting midsommar = new Meeting(6, 26, "Midsommar");
+			calendar.addMeeting(midsommar);
+			// Verify that it was added.
+			Boolean added = calendar.isBusy(6, 26, 0, 23);
+			assertTrue("Midsommar should be marked as busy on the calendar",added);
+		} catch(TimeConflictException e) {
+			fail("Should not throw exception: " + e.getMessage());
+		}
+	}
 	
 }
